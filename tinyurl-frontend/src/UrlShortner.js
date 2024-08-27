b7791aa7d323a4fa7f6182bbdf5e57f04670d429
Poornima Handa <poornimahanda@Poornimas-MacBook-Air.local>
2024-08-27 19:32:57 +0530

    added animator background and css styling

---
+++ tinyurl-frontend/src/UrlShortner.js
@@ -0,0 +1,13 @@
+const UrlShortner = () => {
+  return (
+    <div className="inputContainer">
+       <h1>URL <span>Shortner</span></h1>
+       <div>
+        <input type="text" placeholder="Paste a link to shorten it!"/>
+        <button>shorten</button>
+       </div>
+    </div>
+  )
+}
+
+export default UrlShortner
